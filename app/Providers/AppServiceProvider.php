e5ff473d9ced6ef60bfd61ca241e660aaae556a5
Hafiz Nugraha <manusiakemos@gmail.com>
2021-12-02 13:25:30 +0800

    update config and AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,23 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (config('app.enabled_setting')){
+            Schema::defaultStringLength(190);
+            Paginator::useBootstrap();
 
-        Schema::defaultStringLength(190);
-        Paginator::useBootstrap();
-
-        if (Schema::hasTable("setting")){
-            $settings = Setting::all();
-            if (count($settings)){
-                $configSetting =$settings
-                    ->keyBy('setting_key') // key every setting by its name
-                    ->transform(function ($setting) {
-                        return [
-                            'name' => $setting->setting_name,
-                            'value' => $setting->setting_value,
-                        ]; // return only the value
-                    })
-                    ->toArray();
-                config(['setting' => $configSetting]);
+            if (Schema::hasTable("setting")){
+                $settings = Setting::all();
+                if (count($settings)){
+                    $configSetting =$settings
+                        ->keyBy('setting_key') // key every setting by its name
+                        ->transform(function ($setting) {
+                            return [
+                                'name' => $setting->setting_name,
+                                'value' => $setting->setting_value,
+                            ]; // return only the value
+                        })
+                        ->toArray();
+                    config(['setting' => $configSetting]);
+                }
             }
         }
     }
